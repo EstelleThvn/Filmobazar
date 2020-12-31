7e9927aa1ecbd6471a10b1e542483414f3668bb3
Estelle Thouvenin <estelle.thouvenin00@gmail.com>
2020-12-31 15:35:55 +0100

    finition page du jeu

--- main.js
+++ main.js
@@ -11,20 +11,34 @@ function mixMot(mot){
 
         motArray = motArray.slice(1, motArray.length-1);
       
-        motArray.sort(function() {
-            return 0.5 - Math.random();
-        });  
+        for (let i = motArray.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+
+            // let t = motArray[i]; 
+            // motArray[i] = motArray[j]; 
+            // motArray[j] = t
+            //on permute
+
+            [motArray[i], motArray[j]] = [motArray[j], motArray[i]];
+          }
         mot = firstLetter + motArray.join('') + lastLetter;
     }
     else{
-        motArray.sort(function() {
-            return 0.5 - Math.random();
-        });  
+        for (let i = motArray.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+
+            [motArray[i], motArray[j]] = [motArray[j], motArray[i]];
+          } 
         mot = motArray.join('');
     }
     
   return mot;
 }
+
+let chosenMovieTitle;
+let chosenMovieSynopsis;
+let chosenMoviePoster;
+let chosenMovieReleaseDate;
 
 async function getMovie(){
     const randomNumberPage = Math.floor((Math.random() * 10) + 1);
@@ -39,9 +53,15 @@ async function getMovie(){
 
     console.log(results.results[randomNumberMovie]);
 
-    const chosenMovieTitle = results.results[randomNumberMovie].title;
+    chosenMovieTitle = results.results[randomNumberMovie].title;
 
     console.log(chosenMovieTitle);
+
+    chosenMovieSynopsis = results.results[randomNumberMovie].overview;
+    console.log(chosenMovieSynopsis);
+    chosenMoviePoster = results.results[randomNumberMovie].poster_path;
+    chosenMovieReleaseDate = changeDateFormat(results.results[randomNumberMovie].release_date);
+
 
     let chosenMovieTitleArray = chosenMovieTitle.split(' ');
     console.log(chosenMovieTitleArray);
@@ -65,17 +85,8 @@ async function getMovie(){
 getMovie();
 
 
-// async function getModifiedMovieTitle(){
-//     const request = await fetch(`https://www.dictionaryapi.com/api/v3/references/ithesaurus/json/${titleLowercase}?key=c8dea4be-5e38-451e-86c7-6a1e2a2e9221`);
-//     const results = await request.json();
-
-//     console.log(results);
-
-// }
-
-
-
-
+let nbUserTries = 0;
+let nbRightAnswer = 0;
 
 
 const btn = document.querySelector('form');
@@ -88,13 +99,161 @@ const verifyAnswer = async (e) => {
 
     if(titleLowercase == userAnswer) {
         console.log("ce booooooon");
-        const verificationRight = document.querySelector('.verification');
-        verificationRight.innerHTML = "c'est bon !";
+        nbRightAnswer++
+
+        if(nbRightAnswer === 1) {
+            showMovieDetails();
+        }
+        
+        if(nbUserTries===2){
+            document.querySelector('.indice').innerHTML = ``;
+            // const indice = document.querySelector('.indice');
+            // if(indice !== null){
+            // indice.parentNode.removeChild(indice);
+            // }
+        }
+    }
+    else if (userAnswer == 0) {
+        console.log("c'est pas une réponse ça.");
     }
     else{
         console.log("pa bon.");
-        const verificationFalse = document.querySelector('.verification');
-        verificationFalse.innerHTML = "c'est pas bon. retry !";
+        nbUserTries++;
+        console.log(nbUserTries);
+
+        if(nbUserTries === 1){
+            document.querySelector('.essais').children[0].classList.add("red-cross-container");
+        }
+        else if(nbUserTries === 2) {
+            document.querySelector('.indice').innerHTML = `<p>Hint</p>
+            <p>${chosenMovieSynopsis}</p>`;
+
+            // let newDivIndice = document.createElement("div");
+            // newDivIndice.classList.add("indice");
+            // newDivIndice.innerHTML = `<p>Hint</p>
+            // <p>${chosenMovieSynopsis}</p>`;
+            // document.querySelector('section').appendChild(newDivIndice);
+    
+            document.querySelector('.essais').children[1].classList.add("red-cross-container");
+        }
+        else if(nbUserTries === 3) {
+            console.log("afficher la réponse");
+            document.querySelector('.indice').innerHTML = ``;
+            // const indice = document.querySelector('.indice');
+            // if(indice !== null){
+            // indice.parentNode.removeChild(indice);
+            // }
+            showMovieDetails();
+            document.querySelector('.essais').children[2].classList.add("red-cross-container");
+        }
     }
 }
 btn.addEventListener('submit', (e) => {verifyAnswer(e)});
+
+
+
+const btnChangerPhrase = document.querySelector('.regenere');
+console.log(btnChangerPhrase);
+const ChangeTitle = async (e) => {
+    e.preventDefault();
+
+    document.querySelector('form input[type="text"]').value = '';
+    nbUserTries = 0;
+    nbRightAnswer = 0;
+
+    document.querySelector('.infos-film').innerHTML = ``;
+    // const infosFilm = document.querySelector('.infos-film');
+    // if(infosFilm !== null){
+    //     infosFilm.parentNode.removeChild(infosFilm);
+    // }
+
+    // const indice = document.querySelector('.indice');
+    // if(indice !== null){
+    //     indice.parentNode.removeChild(indice);
+    // }
+
+    document.querySelector('.indice').innerHTML = ``;
+
+    getMovie();
+
+    document.querySelector('.essais').children[0].classList.remove("red-cross-container");
+    document.querySelector('.essais').children[1].classList.remove("red-cross-container");
+    document.querySelector('.essais').children[2].classList.remove("red-cross-container");
+}
+btnChangerPhrase.addEventListener('click', (e) => {ChangeTitle(e)});
+
+
+function showMovieDetails(){
+        document.querySelector('.infos-film').innerHTML = `<img src="https://image.tmdb.org/t/p/w500${chosenMoviePoster}">
+        <div class="text-infos-film">
+        <h1>${chosenMovieTitle}</h1>
+        <div>
+        <p>Release date</p>
+        <p>${chosenMovieReleaseDate}</p>
+        </div>
+        <p>${chosenMovieSynopsis}</p>
+        </div>`;
+
+        // let newDivInfosFilm = document.createElement("div");
+        // newDivInfosFilm.classList.add("infos-film");
+        // newDivInfosFilm.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${chosenMoviePoster}">
+        // <div class="text-infos-film">
+        // <h1>${chosenMovieTitle}</h1>
+        // <div>
+        // <p>Release date</p>
+        // <p>${chosenMovieReleaseDate}</p>
+        // </div>
+        // <p>${chosenMovieSynopsis}</p>
+        // </div>`;
+        // document.querySelector('section').appendChild(newDivInfosFilm);
+}
+
+
+function changeDateFormat(date){
+    let dateArray = date.split('-').reverse();
+
+    switch (dateArray[1]) {
+        case '01':
+            dateArray[1] = 'January';
+          break;
+        case '02':
+            dateArray[1] = 'February';
+          break;
+        case '03':
+            dateArray[1] = 'March';
+        break;
+        case '04':
+            dateArray[1] = 'April';
+        break;
+        case '05':
+            dateArray[1] = 'May';
+        break;
+        case '06':
+            dateArray[1] = 'June';
+        break;
+        case '07':
+            dateArray[1] = 'July';
+        break;
+        case '08':
+            dateArray[1] = 'August';
+        break;
+        case '09':
+            dateArray[1] = 'September';
+        break;
+        case '10':
+            dateArray[1] = 'October';
+        break;
+        case '11':
+            dateArray[1] = 'November';
+        break;
+        case '12':
+            dateArray[1] = 'December';
+        break;
+      }
+
+    console.log(dateArray);
+
+    date = dateArray.join(' ');
+
+    return date;
+}
